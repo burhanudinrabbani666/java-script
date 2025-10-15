dd2990a0bba1d45c29e1cf8bc28f083a40c33124
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-10-15 07:57:52 +0700

    angkot4

--- 6.0/for/index.js
+++ 6.0/for/index.js
@@ -1,33 +1,33 @@
-// for (let index = 1; index <= 10; index++) {
-// console.log(index);
-// }
-//
-// console.log("------");
-//
-// for (let index = 2; index <= 20; index += 2) {
-// console.log(index);
-// }
-//
-// console.log("------");
-//
-// let total = 0;
-// for (let index = 0; index <= 5; index++) {
-// total += index;
-// }
-// console.log(total);
-//
-// console.log("------");
-//
-// let a = 5;
-// let b = 0;
-// for (let index = 1; index <= 10; index++) {
-// b = index * a;
-// console.log(`${index} x ${a} = ${b}`);
-// }
-
-var angka = 10;
-if (angka % 2 == 0) {
-  console.log(`${angka} adalah genap`);
-} else {
-  console.log(`${angka} adalah ganjil`);
+for (let index = 1; index <= 10; index++) {
+  console.log(index);
 }
+
+console.log("------");
+
+for (let index = 2; index <= 20; index += 2) {
+  console.log(index);
+}
+
+console.log("------");
+
+let total = 0;
+for (let index = 0; index <= 5; index++) {
+  total += index;
+}
+console.log(total);
+
+console.log("------");
+
+let a = 5;
+let b = 0;
+for (let index = 1; index <= 10; index++) {
+  b = index * a;
+  console.log(`${index} x ${a} = ${b}`);
+}
+
+// var angka = 10;
+// if (angka % 2 == 0) {
+//   console.log(`${angka} adalah genap`);
+// } else {
+//   console.log(`${angka} adalah ganjil`);
+// }
