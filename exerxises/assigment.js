26a4f78395d7fd8545d2d2402bc4c83091b10925
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-12 21:02:04 +0700

    feat :  strings part 1 assignment

--- exerxises/assigment.js
+++ exerxises/assigment.js
@@ -511,7 +511,6 @@ console.log(bookMap.size);
 bookMap.has(`author`)
   ? console.log(`The author of the book is known`)
   : undefined;
-*/
 
 // 14.1
 // Convert the first book object from the books array into a Map, and assign it to a firstBookMap variable.
@@ -523,3 +522,32 @@ const firstBookMap = new Map(Object.entries(books[0]));
 for (const [key, Value] of firstBookMap) {
   typeof Value === "number" && console.log(key);
 }
+*/
+
+// 15.1
+//Take the ISBN property of the first book from the books array, and log to the console characters at index 6, 4, 9 and 8. Use bracket notation to access individual characters.
+console.log(books[0].ISBN[6]);
+console.log(books[0].ISBN[4]);
+console.log(books[0].ISBN[9]);
+console.log(books[0].ISBN[8]);
+
+// 15.2
+//Below is the quote variable that stores a string. Find the index of the word 'chess', and log it to the console.
+const quote =
+  "A computer once beat me at chess, but it was no match for me at kick boxing";
+
+console.log(quote.indexOf(`chess`)); //
+
+// 15.3
+// Extract the word "boxing" from the same quote string, and log it to the console.
+console.log(quote.slice(quote.lastIndexOf(` `) + 1));
+
+// 15.4
+// Some authors are noted as "(Contributor)", for example "Julie Sussman (Contributor)". Create a function called isContributor that takes an author's name as an argument, and returns either true (if he's a contributor) of false (if he's not a contributor). The string "(Contributor)" is always the last part of the author's name string.
+
+function isContributor(name) {
+  console.log(name.slice(name.lastIndexOf(` `) + 1) == `(Contributor)`);
+}
+
+isContributor("Julie Sussman (Contributor)");
+isContributor("Robert Sedgewick");
