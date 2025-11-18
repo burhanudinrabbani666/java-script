58e69aa4c0cf987c8921012089d51a5e7204bbf4
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-19 06:47:51 +0700

    Markdown For Project Demo

---
+++ projects/bankist/README.md
@@ -0,0 +1,21 @@
+## Bankist
+
+This project is part of a course I took on Udemy. It's not my original project.
+
+Source : [Section : Working With Array]()
+
+## Topics
+
+- []()
+- []()
+- []()
+- []()
+- []()
+- []()
+- []()
+- []()
+- []()
+- []()
+- []()
+
+## Thank you 🏁
--- projects/bankist/script.js
+++ projects/bankist/script.js
@@ -74,3 +74,15 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = [`a`, `b`, `c`, `d`, `e`];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(1, 3));
+console.log(arr.slice(1, -1));
+console.log(arr.slice());
+
+// Splice
+// console.log(arr.splice(2)); // mutate the origial array
+arr.splice(-1);
+console.log(arr);
