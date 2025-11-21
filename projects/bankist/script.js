28313351ac4b1467a332b1ed1b5898dde5f973b7
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-11-21 11:19:13 +0700

    challenge

--- projects/bankist/script.js
+++ projects/bankist/script.js
@@ -303,3 +303,61 @@ console.log(converTitleCase(`this is a nice title`));
 console.log(converTitleCase(`this is a LONG title but not too long`));
 console.log(converTitleCase(`and here is another title with an EXAMPLE`));
 */
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+// 1.
+dogs.forEach(
+  (dog) => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+
+//2.
+
+const sarahDog = dogs.find((dog) => dog.owners.includes("Sarah"));
+const isEatingToMuch = sarahDog.curFood > sarahDog.recommendedFood;
+console.log(isEatingToMuch);
+
+// 3.
+
+const ownersEatToMuch = dogs.filter((dog) => dog.curFood > dog.recommendedFood);
+const ownersEatToLittle = dogs.filter(
+  (dog) => dog.curFood < dog.recommendedFood
+);
+
+// 4.
+
+console.log(
+  `${ownersEatToMuch.flatMap((dog) => dog.owners).join(` and `)} Eat to much`
+);
+console.log(
+  `${ownersEatToLittle
+    .flatMap((dog) => dog.owners)
+    .join(` and `)} Eat to little`
+);
+
+// 5.
+console.log(dogs.some((dog) => dog.curFood === dog.recommendedFood));
+
+//6.
+console.log(
+  dogs.some(
+    (dog) =>
+      dog.curFood < dog.recommendedFood + dog.recommendedFood * 0.1 &&
+      dog.curFood > dog.recommendedFood - dog.recommendedFood * 0.1
+  )
+);
+
+const okayAmount = dogs.filter(
+  (dog) =>
+    dog.curFood < dog.recommendedFood + dog.recommendedFood * 0.1 &&
+    dog.curFood > dog.recommendedFood - dog.recommendedFood * 0.1
+);
+
+const copyDogs = dogs.slice();
+
+console.log(copyDogs);
