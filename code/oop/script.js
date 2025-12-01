7b8c92b4d30412e0bbceb9f09c3d05b6b747c678
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-01 12:45:23 +0700

    feat: Another Class Example

--- code/oop/script.js
+++ code/oop/script.js
@@ -397,7 +397,6 @@ class Student extends PersonCl {
 const martha = new Student("Martha Jonas", 2002, "Art");
 martha.introduce();
 martha.clacAge();
-*/
 
 // Create Object
 const PersonProto = {
@@ -430,3 +429,51 @@ const jay = Object.create(StudentProto);
 jay.init("Jay", 2001, "Law");
 jay.introduce();
 jay.calcAge();
+*/
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movement = [];
+    this.local = navigator.language;
+
+    console.log(`Thanks to opening an account. ${this.owner}`);
+  }
+
+  deposit(val) {
+    this.movement.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan() {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+console.log(acc1);
+
+// Dont do manually ❗
+// acc1.movement.push(250);
+// acc1.movement.push(-140);
+
+// make method first ✅
+acc1.deposit(250);
+acc1.withdraw(333);
+
+acc1.requestLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
