56cfeaedf9acf1ebfad271f4395bef605639a4de
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-03 15:10:29 +0700

    feat: AJAX call

--- code/async/starter/script.js
+++ code/async/starter/script.js
@@ -5,9 +5,42 @@ const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
-// https://countries-api-836d.onrender.com/countries/
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+// OLD school
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const langu = Object.values(data.languages);
+    const name = Object.values(data.currencies)[0].name;
+
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.official}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${langu}</p>
+          <p class="country__row"><span>💰</span>${name}</p>
+        </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('indonesia');
+getCountryData('usa');
--- notes/async/ajax-call.md
+++ notes/async/ajax-call.md
@@ -1,1 +1,36 @@
 ## First AJAX call: XMLHTTPREQUEST
+
+```js
+// Old School
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    const langu = Object.values(data.languages);
+    const name = Object.values(data.currencies)[0].name;
+
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.official}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${langu}</p>
+          <p class="country__row"><span>💰</span>${name}</p>
+        </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData("indonesia");
+getCountryData("usa");
+```
