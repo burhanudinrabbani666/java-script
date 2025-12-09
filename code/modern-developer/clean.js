330bb45f26557c87854518f320e3c38be3012328
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-09 17:16:11 +0700

    feat: add clean.js for budget management and expense tracking functionality

---
+++ code/modern-developer/clean.js
@@ -0,0 +1,51 @@
+const budget = [
+  { value: 250, description: "Sold old TV 📺", user: "jonas" },
+  { value: -45, description: "Groceries 🥑", user: "jonas" },
+  { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
+  { value: 300, description: "Freelancing 👩‍💻", user: "jonas" },
+  { value: -1100, description: "New iPhone 📱", user: "jonas" },
+  { value: -20, description: "Candy 🍭", user: "matilda" },
+  { value: -125, description: "Toys 🚂", user: "matilda" },
+  { value: -1800, description: "New Laptop 💻", user: "jonas" },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = (user) => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = "jonas") {
+  user = user.toLowerCase();
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+
+addExpense(10, "Pizza 🍕");
+addExpense(100, "Going to movies 🍿", "Matilda");
+addExpense(200, "Stuff", "Jay");
+// --------------------- //
+
+const checkExpenses = function () {
+  for (const entry of budget)
+    if (entry.value < getLimit(entry.user)) entry.flag = "limit";
+};
+checkExpenses();
+
+const bigExpenses = function (bigLimit) {
+  let output = "";
+  for (const entry of budget) {
+    output +=
+      entry.value <= -bigLimit
+        ? `${entry.description.slice(-2)} / ` // Emojis are 2 chars
+        : "";
+  }
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+
+console.log(budget);
+bigExpenses(500);
