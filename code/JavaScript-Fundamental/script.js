934c09592bdf30cda2a93fb84b0973cc3c4dcb5a
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-11 08:44:21 +0700

    feat: Documentation about data types

--- code/JavaScript-Fundamental/script.js
+++ code/JavaScript-Fundamental/script.js
@@ -1,3 +1,4 @@
+/*
 console.log("jonas"); // Jonas is valeu
 console.log(21); // 21 is valeu
 
@@ -7,3 +8,13 @@ let firstName = "Burhanudin";
 const lastName = "Rabbani";
 
 console.log(Greeting, firstName, lastName); // Hello, Burhanudin Rabbani
+*/
+
+const javaScriptIsFun = true;
+console.log(javaScriptIsFun); // true
+
+// Check Type of Data Types
+console.log(typeof "Burhanudin Rabbani"); // String
+console.log(typeof 1411); // Number
+console.log(typeof true); // Boolean
+console.log(typeof { firsName: "Burhanudin" }); // Object
